c1749522e694be6022ce951871c2d0eb2d96c212
AHO0 <127205828+AHO0@users.noreply.github.com>
2025-09-15 01:09:07 +0800

    Add files via upload

--- src/main/java/top/warmc/country/town/chunk/Operation.java
+++ src/main/java/top/warmc/country/town/chunk/Operation.java
@@ -11,24 +11,37 @@ import top.warmc.country.CountryMod;
 import top.warmc.country.core.classes.Country;
 import top.warmc.country.core.classes.Town;
 import top.warmc.country.core.pool.CountryPool;
-
+//圈地相关
 @Mod.EventBusSubscriber(modid = CountryMod.MODID, value = Dist.CLIENT)
 public class Operation {
 	public static void Statement(Player player, boolean isAuto) {
+        //防止空指针异常
         if (player == null) return;
-
+        //依旧检测
+        //获取信息
         Town town = CountryPool.getTownFromPlayer(player);
-        if (town == null && !isAuto) { player.displayClientMessage(Component.literal("§6[Country]§3[player]§c您没有建立城镇!"), false); return; }
-
-        if (CountryPool.getTownFromLand(player.level(), player.level().getChunk(player.getOnPos())) != null && !isAuto) { player.displayClientMessage(Component.literal("§6[Country]§a[chunk]§c该区块已被声明!"), false); return; }
-
+        //检测是否在城镇中
+        if (town == null && !isAuto) {
+            player.displayClientMessage(Component.literal("§6[Country]§3[player]§c您没有建立城镇!"), false);
+            return;
+        }
+        //检测区块状态
+        if (CountryPool.getTownFromLand(player.level(), player.level().getChunk(player.getOnPos())) != null && !isAuto) {
+            player.displayClientMessage(Component.literal("§6[Country]§a[chunk]§c该区块已被声明!"), false);
+            return;
+        }
+        //
         Country country = CountryPool.getCountryFromTown(town);
+        //防止空指针异常
         if (country == null) return;
+        //检测国家状态
         if (country.isPeace()) {
             town.getLand().add(player.level(), player.level().getChunk(player.getOnPos()));
 
             if (!isAuto)player.displayClientMessage(Component.literal("§6[Country]§a[chunk]§e声明成功!"), false);
-        } else {
+        }
+        //这几把else了个啥
+        else {
 
         }
     }
